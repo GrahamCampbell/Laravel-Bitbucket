42e1356b3d49b11454e13e8c7a1e7f4cc2705bb8
Graham Campbell <graham@alt-three.com>
2017-12-31 17:28:14 +0000

    Fixed config in phars

--- src/BitbucketServiceProvider.php
+++ src/BitbucketServiceProvider.php
@@ -45,7 +45,7 @@ class BitbucketServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/bitbucket.php');
+        $source = realpath($raw = __DIR__.'/../config/bitbucket.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('bitbucket.php')]);
